a50ea706ccbb3ea95e0ccb5b0bfd5f5558e11acd
gyuseon <mn5592@naver.com>
2023-08-26 23:35:07 +0900

    AOP가 필요한 상황 ~ AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,10 +20,10 @@ public class MemberService {
     /**
      * 회원가입
      */
-    public Long join(Member member){
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+    public Long join(Member member) {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -36,8 +36,8 @@ public class MemberService {
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
+    public List<Member> findMembers() {
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
